29a5a58025cf4365bf2b74aa7c4d6a09d5913dc8
anastanvir <anas.seoalchemist@gmail.com>
2024-12-09 19:03:30 +0500

    Update

---
+++ Lecture-11/script.js
@@ -0,0 +1,23 @@
+// Case Senstivity 
+
+// var a = 10;
+// var a = 10;
+
+
+// let a = 10;
+// let a = 10;
+
+// const a = 10;
+// const a = 10;
+
+// Operators 
+
+
+
+
+const a = 10;
+const b = 25;
+
+
+
+
